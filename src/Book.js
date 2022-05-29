3f3e0cfc6424393ecba2f0acbadc82ccd9db9681
chagraCome <amriasma2016@gmail.com>
2022-05-29 20:59:08 +0200

    break the code of app into different components
    create Book component,
    bookShelf component,
    and make two pages into different component; listBooks and search page

---
+++ src/Book.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Component } from 'react'
+class Book extends Component{
+    render(){
+        return <div>
+            Book
+        </div>
+    }
+}
+export default Book
---
+++ src/BookShelf.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Component } from 'react'
+class BookShelf extends Component{
+    render(){
+        return <div>
+           BookShelf
+        </div>
+    }
+}
+export default BookShelf
